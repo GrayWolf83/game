e5fa749eb2e46dce0b3273500d574d03f2425de7
Sergey Bernyakovich <sb_aktau@bk.ru>
2021-11-23 11:12:21 +0500

    Создано игровое поле

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 import { Info } from './info'
+import { GameArea } from './gameArea'
 
 export class App {
     #wrapp
     #info
+    #area
 
     constructor() {
         this.#wrapp = document.createElement('div')
         this.#info = new Info('Крестики - Нолики')
+        this.#area = new GameArea()
     }
 
     #wrappConfigHandler() {
         this.#wrapp.classList.add('wrapp')
-        this.#wrapp.append(this.#info.run())
+        this.#wrapp.append(this.#info.run(), this.#area.run())
     }
 
     run() {
